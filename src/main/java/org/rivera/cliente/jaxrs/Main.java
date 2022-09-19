d038236a7d3d5b4ce78780232099c72f8a430266
irvinzato <multizato@hotmail.com>
2022-09-18 22:42:08 -0500

    Creacion, edicion y eliminacion implementados, cabeceras en request

--- src/main/java/org/rivera/cliente/jaxrs/Main.java
+++ src/main/java/org/rivera/cliente/jaxrs/Main.java
@@ -2,6 +2,7 @@ package org.rivera.cliente.jaxrs;
 
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.MediaType;
@@ -31,10 +32,7 @@ public class Main {
     System.out.println(response.getMediaType());
 
     System.out.println("========== Lista de Cursos ==========");
-    List<Curso> curses = rootUri.request(MediaType.APPLICATION_JSON)
-            .get(Response.class)
-            .readEntity(new GenericType<List<Curso>>(){});
-    curses.forEach(System.out::println);
+    getCourses(rootUri);
 
     System.out.println("========== Lista de Cursos, de otra forma ==========");
     Response response2 = rootUri.request(MediaType.APPLICATION_JSON)
@@ -43,5 +41,45 @@ public class Main {
     curses2.forEach(System.out::println);
     System.out.println(response2.getDate());
 
+    System.out.println("========== Creación de Curso ==========");
+    Curso newCourse = new Curso();
+    newCourse.setName("FlexBox");
+    newCourse.setTeacher("Irving");
+    newCourse.setDescription("La mejor herramienta para paginas responsivas");
+    newCourse.setDuration(35D);
+
+    Entity<Curso> entityHeader = Entity.entity(newCourse, MediaType.APPLICATION_JSON);
+    course = rootUri.request(MediaType.APPLICATION_JSON)
+            .post(entityHeader, Curso.class);
+    System.out.println("Curso guardado: " + course);
+    getCourses(rootUri);
+
+    System.out.println("========== Editar Curso ==========");
+    Curso editCourse = course;
+    editCourse.setDuration(39D);
+    entityHeader = Entity.entity(editCourse, MediaType.APPLICATION_JSON);
+    course = rootUri.path("/" + editCourse.getId())
+            .request(MediaType.APPLICATION_JSON)
+            .put(entityHeader, Curso.class);
+    System.out.println("Curso editado: " + course);
+    getCourses(rootUri);
+
+    System.out.println("========== Eliminar Curso ==========");
+    rootUri.path("/6")
+            .request()
+            .delete();
+    getCourses(rootUri);
+
+
   }
+
+
+  private static void getCourses(WebTarget rootUri) {
+    System.out.println("========== Lista actualizada ==========");
+    List<Curso> courses = rootUri.request(MediaType.APPLICATION_JSON)
+            .get(Response.class)
+            .readEntity(new GenericType<List<Curso>>(){});
+    courses.forEach(System.out::println);
+  }
+
 }
